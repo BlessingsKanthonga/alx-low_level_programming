c09db2d2c39dcacdf3197560f3a81079c5645cb8
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-03-17 05:48:04 -0700

    chnages to positive or negative file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,17 +14,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	if(n>0)
+	if(n > 0)
 	{
-		printf("%d\n is positive\n", n);
+		printf("%d\n is positive", n);
 	}
-	if(n==0)
+	if(n == 0)
 	{
-		printf("%d\n is zero\n", n);
+		printf("%d\n is zero", n);
 	}
-	if(n<0)
+	if(n < 0)
 	{
-		printf("%d\n is negative\n", n);
+		printf("%d\n is negative", n);
 	}
 
 	return (0);
