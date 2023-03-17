b742a6710aaf8bb31ed72f9d588c4fd5ca5cbc93
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-03-17 10:00:43 -0700

    Maintaining the betty type of formatting

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,20 +10,25 @@
 int main(void)
 {
 	int n;
+	int x;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	printf("Last digit of %d is %d ", n, n % 10);
-	if ( (n % 10) > 5 )
+	x = n;
+	if(n < 0)
+	{
+		x = n * -1;
+	}
+	printf("Last digit of %d is %d ", n, x % 10);
+	if ((x % 10) > 5)
 	{
 		printf("and is greater than 5\n");
 	}
-	if ( (n % 10) == 0 )
+	if ((x % 10) == 0)
 	{
 		printf("and is 0\n");
 	}
-	if( ((n % 10) < 6) && ((n % 10) != 0) )
+	if(((x % 10) < 6) && ((x % 10) != 0))
 	{
 		printf("and is less than 6 and not 0\n");
 	}
