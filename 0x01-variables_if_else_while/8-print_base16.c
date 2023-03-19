34857974348a323f7e4b401d5a697bdc61955d4a
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-03-19 02:05:26 -0700

    print base 16 numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - print combinations of numbers
- * void: no parameters
+ * main - prints base 16 numbers
+ * void: takes no parameters
  * Return: 0
  */
 
@@ -13,7 +13,10 @@ int main(void)
 	for (numbers = 0; numbers < 10; numbers++)
 	{
 		putchar('0' + numbers);
-
+	}
+	for (numbers = 0; numbers < 6; numbers++)
+	{
+		putchar('a' + numbers);
 	}
 	putchar('\n');
 	return (0);
