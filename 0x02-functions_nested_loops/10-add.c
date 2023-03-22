1e2206541af2868ed89d68966451292ef3954626
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-03-22 10:16:06 -0700

    function 10-1dd.c which adds and prints integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds and prints integers
+ * int, int: takes integer parameters
+ * @c1: parameters 1
+ * @c2: parameter 2
+ * Return: returns an integer
+ */
+
+int add(int c1, int c2)
+{
+	int result = c1 + c2;
+	return (result);
+}
