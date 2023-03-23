31cf527e52875392219b9133ee8d5d812e7926ab
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-03-23 12:36:03 -0700

    multiplication file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies integers
+ * @a: first parameter
+ * @b: substitute for second variable
+ * Return: result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
