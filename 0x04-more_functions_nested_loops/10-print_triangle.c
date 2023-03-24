9e98faefb5d19955a76c93a57611e56b189a3989
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-03-24 07:09:55 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: parameter for inputting variable
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int number5 = size;
+	int number;
+	int number6 = 1;
+	int number4;
+	int number3;
+
+	for (number = size; number > 0; number--)
+	{
+		if ((size == 0) || (size < 0))
+		{
+			_putchar('\n');
+			break;
+		}
+
+		else if (size > 0)
+		{
+			while (number6 <= size)
+			{
+				number3 = number5;
+				number4 = number6;
+				while (number3 > 0)
+				{
+					_putchar(' ');
+					number3--;
+				}
+				while (number4 <= size)
+				{
+					_putchar(35);
+					number4++;
+				}
+				_putchar('\n');
+				number5--;
+				number6++;
+			}
+		}
+	}
+}
