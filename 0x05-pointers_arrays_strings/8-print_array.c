2597c6b3387e8ce385711363979a3f5c054414b1
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-03-28 07:38:32 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints some array elements
+ * int *: first input is of type int pointer
+ * int: second input is of type int
+ * @a: sub for first input (array)
+ * @n: sub for second input (Size)
+ */
+
+void print_array(int *a, int n)
+{
+	int counter;
+
+	for (counter = 0; counter < n; counter++)
+	{
+		if (counter != (n - 1))
+		{
+			printf("%d, ", a[counter]);
+		}
+		else
+			printf("%d\n", a[counter]);
+	}
+}
