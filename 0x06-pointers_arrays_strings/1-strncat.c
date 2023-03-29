1ff50d593a1f895b7019c47808c976e191af72b5
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-03-29 02:51:26 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * char \*: 1st input type
+ * char \*: 2nd input type
+ * @dest: first input sub
+ * @src: 2nd input sub
+ * Return: char variable
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0;
+	int count2 = 0;
+	char check = '1';
+
+	while (check != '\0')
+	{
+		if (dest[count] == '\0')
+		{
+			while ((src[count2] != '\0') && (count2 < n))
+			{
+				dest[count] = src[count2];
+				count2++;
+				count++;
+			}
+			check = '\0';
+
+			if (src[count2] == '\0')
+				dest[count] = src[count2];
+		}
+		else
+			count++;
+	}
+	return (dest);
+}
