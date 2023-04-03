0d1113f3834605193cc833616f74913135edefb8
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-03 04:45:48 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - replaces value pointed to with cpnstant btye
+ * @s: pointer to cnstant variable
+ * @b: constant byte
+ * @n: number of addresses for constant byte
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int count;
+
+	for (count = 0; count < n; count++)
+	{
+		s[count] = b;
+	}
+	return (s);
+}
