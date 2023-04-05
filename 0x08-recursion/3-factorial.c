cd65c3faec7e253ee97c542e205c01ef102a0d23
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-05 06:44:48 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - factorial
+ * @n: input
+ * Return: factorial answer
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else 
+		return (n * factorial(n - 1));
+}
