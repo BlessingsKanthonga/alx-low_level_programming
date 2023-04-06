20b9082b4b4ed7b868eaeccffd4183fabb12e265
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-05 23:10:28 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - square roog of a number
+ * @n: number to be square rooted
+ * Return: square root or -1 if no square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (find_sqrt(n, 1));
+}
+
+/**
+ * sqrt - calculates square root square root for _sqrt_recursion
+ * @n: square rooting number
+ * @x: starting point for sqiaring
+ * Return: square root to _sqrt_recursion or -1
+ */
+
+int find_sqrt(int n, int x)
+{
+	if (n == 0)
+		return (0);
+	else if ((x * x) == n)
+		return (x);
+	else if ((x * x) > n)
+		return (-1);
+	else
+		return (find_sqrt(n, (x + 1)));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,6 @@ int _pow_recursion(int, int);
 int _sqrt_recursion(int);
 int is_prime_number(int);
 int calculate_prime(int, int);
+int find_sqrt(int, int);
 
 #endif /* MAIN_H */
