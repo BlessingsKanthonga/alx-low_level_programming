e4a7350768495cf28af915fbd6f36f65615e1dbe
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-07 08:19:50 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - count arguments
+ * @argc: count of arguments
+ * @argv: array of argumens
+ * Return: 0 if success
+ */
+
+int main (int argc, char *argv[])
+{
+	if (*argv != NULL)
+		printf("%d\n", argc - 1);
+	return (0);
+}
