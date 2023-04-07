2c42e69558fd8300ab859058311b8f7ff4fff84c
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-07 07:58:54 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints program name
+ * @argc: count of arguments
+ * @argv: array of arguments
+ * Return: 1 if success else 0 if errors
+ */
+
+int main (int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
