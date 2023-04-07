dcdead712f6ba346f803a0bf0e205e2455c59275
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-07 10:44:08 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies args
+ * @argc: counts argumentx
+ * @argv: arguments array
+ * Return: 0 if success 1 if not two arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+	int one;
+	int two;
+
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	else
+	{
+		one = atoi(argv[1]);
+		two = atoi(argv[2]);
+		result = one * two;
+		printf("%d\n", result);
+		return (0);
+	}
+}
