cc189a4e1e68f79123eaebe64bae2bec91e00de3
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-09 12:13:51 -0700

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-ar rc liball.a *.o
+for file in *.c;
+do
+	gcc -c $file -o $file.o;
+done
+
+for file in *.c.o;
+do
+	mv -- "$file" "${file%.c.o}.o";
+done
+
+for file in *.o;
+do
+	ar rc liball.a $file;
+done
