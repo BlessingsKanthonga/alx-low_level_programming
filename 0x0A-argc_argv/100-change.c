693e8aedfe69f7f478cffcac541481bc54fff802
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-09 09:15:37 -0700

    100-change.c if argumets not exactly 1

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	int result = 0;
 	int coin[] = {25, 10, 5, 2, 1};
 
-	if ((argv[1] == NULL) || (argc > 2))
+	if (argc != 2)
 	{
 		printf("ERROR\n");
 		return (1);
