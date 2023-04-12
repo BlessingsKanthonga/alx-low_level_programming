fa206895f7c3f7118db3cd9d9a92dd03a6fb51c1
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-12 13:39:07 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates dynamic aray
+ * @size: array size
+ * @c: initializer
+ * Return: null if empty else array pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int count;
+
+	if (size == 0)
+		return (NULL);
+	else
+	{
+		array = (char *) malloc(sizeof(char) * size);
+
+		if (array == NULL)
+			return (NULL);
+		for (count = 0; count < size; count++)
+			array[count] = c;
+	}
+	return (array);
+}
