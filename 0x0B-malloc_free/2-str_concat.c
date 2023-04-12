b632f3faf32ed934926138fda09a1adcd07e44b0
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-12 15:55:57 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * str_concat - concats 2 strings
+ * @s1: string1
+ * @s2: string 2
+ * Return: strng or null for failure
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int count = 0;
+	int counter;
+	int counter2;
+	int count2 = 0;
+	int count3 = 0;
+	int length = 0;
+	char *con;
+
+	if ((s1 == NULL) && (s2 == NULL))
+		return (NULL);
+	while (s1[count] != '\0')
+	{
+		count++;
+		length++;
+	}
+	while (s2[count2] != '\0')
+	{
+		count2++;
+		length++;
+	}
+	con = (char *) malloc(sizeof(char) * (length + 1));
+
+	if (con == NULL)
+		return (NULL);
+	for (counter = 0; counter < count; counter++)
+		con[counter] = s1[counter];
+	for (counter2 = counter; counter2 < length; counter2++)
+	{
+		con[counter2] = s2[count3];
+		count3++;
+	}
+	con[length] = '\0';
+	return (con);
+}
