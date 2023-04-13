f2f490b34278fee2aa7f4c3bf0b8690ed7d0fac0
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-13 11:27:32 -0700

    changes to files

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+void *malloc_checked(unsigned int);
+char *string_nconcat(char *, char *, unsigned int);
+void *_calloc(unsigned int, unsigned int);
+int *array_range(int, int);
+
+#endif /* MAIN_H */
