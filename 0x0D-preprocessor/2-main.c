f6752514e7ef459b47f24a816f1717c90bf442bb
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-17 13:23:06 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints  name of program file
+ * void: no parameters
+ * Return: void
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
