c2243fd5256f78a29f29f8749e11974b12c6bd3d
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-18 12:43:38 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dog d variables from meemory
+ * @d: keeper of dog variables
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d -> name);
+		free(d -> owner);
+		free(d);
+	}
+}
