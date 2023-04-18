26442967fda45478d753f7d3b7ff061823de7c80
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-18 07:49:54 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialises dog struct
+ * @d: keeper of dog variables
+ * @name: dog name
+ * @age: of dog
+ * @owner: of dog d
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
