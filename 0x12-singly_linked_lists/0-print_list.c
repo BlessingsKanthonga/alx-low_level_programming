5bc35c8b0a350ecd6fc2489cc721dd70bffcc473
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-27 05:06:40 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
 
 /**
  * print_list - prints elements of list_t
- * @h: list
+ * @h: struct
  * Return: number of nodes
  */
 
@@ -14,11 +14,11 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
+		if (h -> str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+			printf("[%d] %s\n", h -> len, h -> str);
+		h = h -> next;
 		count++;
 	}
 
