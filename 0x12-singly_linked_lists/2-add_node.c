276431be39cf3f82e656fda49c149d72e894b1bf
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-27 07:54:12 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - adds node to linked list
+ * @head: pointer to struct
+ * @str: string to be copied
+ * Return: addres of new element or NULL if failure
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int count = 0;
+	unsigned int length = 0;
+	list_t *node = malloc(sizeof(list_t));
+
+	if (node == NULL)
+		return (NULL);
+	if (str == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node -> str = strdup(str);
+	
+	while (str[count] != '\0')
+	{
+		length++;
+		count++;
+	}
+
+	node -> len = length;
+	node -> next = *head;
+	*head = node;
+	return (node);
+}
