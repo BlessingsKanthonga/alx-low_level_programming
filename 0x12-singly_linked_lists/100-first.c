f305e8a847646537101bff15aca527b2b6701d1a
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-27 12:43:20 -0700

    100-first.c first.h

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "first.h"
 
 /**
  * first - runs before main fn
  */
 
-void first(void) __attribute__((constructor));
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
