c56e2488268d5a9ab33f9b0d46302eed1e2d132a
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-27 06:56:57 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * list_len - prints number of elements
+ * @h: struct
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h -> str == NULL)
+			break;
+		h = h -> next;
+		count++;
+	}
+
+	return count;
+}
