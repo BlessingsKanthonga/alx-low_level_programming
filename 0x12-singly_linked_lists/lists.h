394eb0b281025a3ac4022a0e321078ab4f875c9f
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-27 12:10:02 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -21,5 +22,6 @@ list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 int _putchar(char);
+void free_list(list_t *head);
 
 #endif /* LISTS_H */
