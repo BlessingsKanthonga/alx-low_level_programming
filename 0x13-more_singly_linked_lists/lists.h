6835505114c01f038cd5a5972232159f3969a17b
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-29 19:42:18 -0700

    Hare and Tortoise algorithm Lol phew

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,6 +15,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+listint_t *find_listint_loop(listint_t *head);
 void free_listint(listint_t *head);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *add_nodeint(listint_t **head, const int n);
