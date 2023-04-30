d8148666230520c1fadb3fa9496ab8ac80beb155
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-30 03:03:07 -0700

    function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTION_POINTERS_H
 #include <stddef.h>
 
+void print_opcodes(int n);
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
