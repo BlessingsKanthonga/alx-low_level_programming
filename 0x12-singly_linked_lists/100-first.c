7a72aad3f895c79af293a0f0f552ec583217ccb1
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-30 13:16:49 -0700

    corrctions to 0x12

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
  * first - runs before main fn
  */
 
-void first(void) __attribute__((constructor));
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -17,6 +17,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+void first(void) __attribute__((constructor));
 list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
