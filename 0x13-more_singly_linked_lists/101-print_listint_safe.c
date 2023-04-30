bdb019e926cc1e37380a5329788e7cc1cb5feea5
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-29 18:51:03 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,37 +3,49 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list of integers safely
- * @head: pointer to the beginning of the linked list
+ * print_listint_safe - prints a linked list safely
+ * @head: pointer to the head of the list
  * Return: number of nodes in the list
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *slow;
-	const listint_t *fast;
-
-	if (head == NULL)
-		exit(98);
-	slow = head;
-	fast = head->next;
+	const listint_t *current = head;
+	const listint_t *visited[1024] = {NULL};
 	
-	while ((fast != NULL) && (fast < slow))
+	while (current != NULL)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
+		printf("[%p] %d\n", (void *)current, current->n);
+		visited[count] = current;
 		count++;
-		slow = slow->next;
-		fast = fast->next;
-		
-		if ((fast != NULL) && (fast < slow))
+
+		if (contains(visited, count, current->next))
 		{
-			fast = fast->next;
-			count++;
+			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+			break;
 		}
+		current = current->next;
 	}
-	printf("[%p] %d\n", (void *)slow, slow->n);
-	count++;
-	
 	return (count);
+}
+
+/**
+ * contains - checks if an array contains a given element
+ * @array: pointer to the array
+ * @size: size of the array
+ * @elem: element to check for
+ * Return: 1 if array contains elem, 0 otherwise
+ */
+
+int contains(const listint_t *array[], size_t size, const listint_t *elem)
+{
+	size_t i;
+	
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == elem)
+			return (1);
+	}
+	return (0);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -26,6 +26,7 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+int contains(const listint_t *array[], size_t size, const listint_t *elem);
 size_t print_listint_safe(const listint_t *head);
 size_t print_listint(const listint_t *h);
 int _putchar(char);
