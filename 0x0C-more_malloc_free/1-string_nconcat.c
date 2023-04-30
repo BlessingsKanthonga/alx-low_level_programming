a6932e1d491b35d06f8460f4f9ea657e6806527b
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-04-30 05:18:39 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,12 +37,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	result = malloc(s1_len + n + 1);
 
 	if (result == NULL)
-		return NULL;
+		return (NULL);
 	for (i = 0; i < s1_len; i++)
 		result[i] = s1[i];
 	for (j = 0; j < n; j++)
-		result[i+j] = s2[j];
-	result[i+j] = '\0';
+		result[i + j] = s2[j];
+	result[i + j] = '\0';
 
 	return (result);
 }
