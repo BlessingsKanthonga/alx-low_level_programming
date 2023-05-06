ba5aa85d296f12378943fdfd2ecc5bfaa22ee842
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-05-06 13:31:58 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,36 +11,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int count = 0;
-	char check = '1';
-	int result;
-	int count2 = 0;
+        while (*s1 && (*s1 == *s2))
+        {
+                s1++;
+                s2++;
+        }
 
-	while (check != '\0')
-	{
-		if ((s1[count] != '\0') && (s2[count] != '\0'))
-		{
-			if (s1[count] == s2[count])
-				count2++;
-		}
-		else if ((s1[count] == '\0') && (s2[count] != '\0'))
-		{
-			result = -15;
-			check = '\0';
-		}
-		else if ((s1[count] != '\0') && (s2[count] == '\0'))
-		{
-			result = 15;
-			check = '\0';
-		}
-		else
-		{
-			result = 0;
-			check = '\0';
-		}
-
-		count++;
-	}
-
-	return (result);
+        return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
