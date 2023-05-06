fa828c08544c15d66cf64a6dcec8831fb408b405
Blessings Kanthonga <blessikanthonga@gmail.com>
2023-05-06 05:52:55 -0700

    7-print_chessboard.c correction

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: the 8x8 chessboard array
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		_putchar('\n');
+	}
+}
